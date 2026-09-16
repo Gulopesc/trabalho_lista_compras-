cdf757a730edc2123eac81c3a8167a0a0f81ee13
Gulopesc <gucardosohematita@gmail.com>
2026-09-16 20:13:01 -0300

    comentarios adicionados

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
     int resposta = -1;
 
     while (resposta != 0) {
+        //tela que aparece pro usuário depois de cada operação com as opções disponiveis
         cout << "\n===== Lista de Compras =====" << endl;
         cout << "1 - Adicionar item na lista" << endl;
         cout << "2 - Remover item da lista" << endl;
@@ -17,8 +18,10 @@ int main() {
         cout << "Digite a opcao correspondente ao comando que deseja executar: ";
         cin >> resposta;
 
+        //switch pra executar cada função de acordo com a alternativa escolhida
         switch (resposta) {
             case 1: {
+                //pega os dados do novo item (impede codigos que já existam) e adiciona à lista
                 item novoItem;
                 do {
                     cout << "Digite o codigo do produto: ";
@@ -40,8 +43,9 @@ int main() {
                 listaCompras.cadastraProduto(novoItem);
                 break;
             }
-
+            
             case 2: {
+                //pergunta ao usuário se ele quer remover o ultimo item ou um item qualquer pelo codigo e executa a função para tal
                 char alternativa = 'z';
                 do {
                     cout << "Digite 'a' para remover o ultimo item da lista ou 'b' para remover um item pelo codigo: ";
@@ -63,6 +67,7 @@ int main() {
             }
 
             case 3: {
+                //pergunta ao usuário se ele quer imprimir toda a lista ou um item qualquer pelo codigo e executa a função para tal
                 char alternativa = 'z';
                 do {
                     cout << "Digite 'a' para consultar toda a lista ou 'b' para consultar um item especifico: ";
@@ -84,6 +89,7 @@ int main() {
             }
 
             case 4:
+                //calcula o valor total dos produtos atualmente na lista
                 listaCompras.calculaTotal();
                 break;
 
@@ -96,106 +102,5 @@ int main() {
                 break;
         }
     }
-
-    //ui basica dando pro usuario as opções que o programa tem
-    do {
-        cout << "1 - Adicionar item na lista" << endl;
-        cout << "2 - Remover item da lista" << endl;
-        cout << "3 - Consultar itens da lista" << endl;
-        cout << "4 - Calcular valor total da lista" <<endl;
-        cout << "0 - Finalizar programa" << endl;
-        cout << "Digite a opcao correspontente ao comando que deseja executar: " << endl;
-        
-        cin >> resposta;
-
-        switch(resposta){
-            //case para inserir itens se a lista não estiver cheia
-            //o código não pode ser repetido
-            case 1:{
-                if (listaCompras.listaCheia()){
-                    cout << "Lista cheia, remova algum produto antes de cadastrar outro." << endl;
-                    break;
-                }
-                item novoItem;
-                do{
-                    cout << "Digite o codigo do produto: ";
-                    cin >> novoItem.codProduto;
-                    if (listaCompras.confereRepetido(novoItem.codProduto))
-                        cout << "Ja ha um produto com esse codigo, digite outro." << endl;
-                }while (listaCompras.confereRepetido(novoItem.codProduto));
-
-                cout << "Digite o nome do produto: ";
-                cin >> ws;
-                getline(cin, novoItem.nome);
-                cout << "Digite a quantidade a ser comprada: ";
-                cin >> novoItem.quantidade;
-                cout << "Digite o valor unitario do produto: ";
-                cin >> novoItem.valor;
-
-                listaCompras.cadastraProduto(novoItem);
-                break;
-            }
-            //case pra englobar as duas formas de remover itens da lista(fim ou qualquer posicao)
-            case 2:{ 
-                char alternativa = 'z';
-                do{
-                    cout << "Digite \"a\" para remover o ultimo item da lista ou \"b\" para remover um item qualquer pelo codigo: ";
-                    cin >> alternativa;
-                    if (alternativa != 'a' && alternativa != 'b')
-                        cout << "Resposta invalida.";
-                }while (alternativa != 'a' && alternativa != 'b');
-
-                if (alternativa == 'a'){
-                    listaCompras.removeUltimo();
-                }
-                if (alternativa == 'b'){
-                    string codigoRemover;
-                    cout << "Digite o codigo do produto que deseja remover: ";
-                    cin >> codigoRemover;
-                    listaCompras.removePorCodigo(codigoRemover);
-                }
-                break;
-            }
-            //case englobando os dois tipos de consulta (todos os elementos ou pelo codigo)
-            case 3:{ 
-                char alternativa = 'z';
-                do{
-                    cout << "Digite \"a\" para consultar toda a lista ou \"b\" para consultar um item especifico: ";
-                    cin >> alternativa;
-                    if (alternativa != 'a' && alternativa != 'b')
-                        cout << "Resposta invalida.";
-                }while (alternativa != 'a' && alternativa != 'b');
-
-                if (alternativa == 'a'){
-                    listaCompras.imprimeLista();
-                }
-                if (alternativa == 'b'){
-                    string codigoImprimir;
-                    cout << "Digite o codigo do produto que deseja visualizar: ";
-                    cin >> codigoImprimir;
-                    listaCompras.imprimePorCodigo(codigoImprimir);
-                }
-                break;
-            }
-            
-            //case pra exibir o valor total da lista
-            case 4:{
-                listaCompras.calculaTotal();
-                break;
-            }
-            //case pra finalizar
-            case 0:{
-                cout << "Programa finalizado." << endl;
-                break;  
-            }
-            //case caso a opção dada pelo usuario não seja reconhecida
-            default:{
-                cout << "Opcao nao reconhecida. " << endl;
-                break;
-            }
-        }
-
-        cout << endl;
-    } while (resposta != 0);
     return 0;
 }
--- produto.cpp
+++ produto.cpp
@@ -1,17 +1,21 @@
 #include "produto.h"
 
+//método para criar a lista, que começa com primeiro e ultimo apontando pra nada e tamanho 0
 ListaCompras::ListaCompras() : primeiro(nullptr), ultimo(nullptr), tam(0) {}
 
+//metodo pra destruir os itens da lista, vai apagando o ultimo até que a lista fique totalmente vazia
 ListaCompras::~ListaCompras() {
     while (!listaVazia()) {
         removeUltimo();
     }
 }
 
+//verifica se a lista está totalmente vazia, olhando se o ponteiro primeiro aponta pra nada (ou seja, não há item algum)
 bool ListaCompras::listaVazia() const {
     return primeiro == nullptr;
 }
 
+//percorre cada item do primeiro ao ultimo usando o ponteiro "Proximo" pra ver se já tem algum item com o codigo
 bool ListaCompras::confereRepetido(string codigo) const {
     for (item* atual = primeiro; atual != nullptr; atual = atual->proximo) {
         if (atual->codProduto == codigo) {
@@ -21,6 +25,8 @@ bool ListaCompras::confereRepetido(string codigo) const {
     return false;
 }
 
+//cadastra um produto com os dados inseridos na main, como ele chega no final da lista o ponteiro do que era o ultimo aponta pra ele e o ponteiro dele aponta pra nada
+//a contagem tambem é atualizada e o ponteiro "ultimo" passa a apontar pra ele
 void ListaCompras::cadastraProduto(const item& novoItem) {
     item* novoPtr = new item;
     *novoPtr = novoItem;
@@ -38,6 +44,9 @@ void ListaCompras::cadastraProduto(const item& novoItem) {
     cout << "Produto cadastrado com sucesso!" << endl;
 }
 
+//se a lista não estiver vazia vai deletar o ultimo item
+//se o ultimo for igual ao primeiro só tem um item e vai deletar ele
+//se não for vai percorrer cada item até chegar no que aponta pro ultimo, então define esse como ultimo e deleta o ultimo
 void ListaCompras::removeUltimo() {
     if (listaVazia()) {
         cout << "Lista vazia, nao ha produtos para remover." << endl;
@@ -65,6 +74,12 @@ void ListaCompras::removeUltimo() {
     cout << "Ultimo item removido." << endl;
 }
 
+//se a lista não estiver vazia vai procurar o item com determinado codigo pra deletar
+//percorre item por item, sempre guardando o item anterior, até chegar no final da lista ou achar o código
+//se achar e for o primeiro, redefine o primeiro como o próximo, se for o ultimo redefine o ultimo como o anterior
+//se estiver no meio, faz o anterior apontar pro próximo
+//em qualquer caso, deleta o atual no final
+//ou, se não achar nenhum item com código, só informa ao usuario e retorna
 void ListaCompras::removePorCodigo(string codigo) {
     if (listaVazia()) {
         cout << "Lista vazia, nao ha produtos para remover." << endl;
@@ -99,6 +114,10 @@ void ListaCompras::removePorCodigo(string codigo) {
     cout << "Produto removido com sucesso." << endl;
 }
 
+//procura e imprime o item com o mesmo codigo que o passado na função (não existem codigos repetidos)
+//se estuver vazio só avisa e retorna
+//se não estiver faz um for pra percorrer toda a lista e imprimir o item com o código, e retornar
+//se chegar no fim sem achar o código, avisa e também retorna
 void ListaCompras::imprimePorCodigo(string codigo) const {
     if (listaVazia()) {
         cout << "Lista vazia, nao ha produtos para procurar." << endl;
@@ -118,6 +137,9 @@ void ListaCompras::imprimePorCodigo(string codigo) const {
     cout << "Produto nao encontrado." << endl;
 }
 
+//imprime toda a lista de compras, todos os itens com seus dados
+//primeiro verifica se tá fazia, se estiver avisa do erro e retorna
+//percorre toda a lista até chegar no fim (quando o ponteiro "proximo") apontar pra nada, e imprime os dados do item
 void ListaCompras::imprimeLista() const {
     if (listaVazia()) {
         cout << "Lista vazia, nao ha produtos para imprimir." << endl;
@@ -134,6 +156,10 @@ void ListaCompras::imprimeLista() const {
     }
 }
 
+//calcula o valor total da lista de acordo com a quantidade e preço de cada item
+//primeiro verifica se não está vazia, se etiver ele avisa e retorna
+//depois percorre item por item até chegar no fim, a cada item ele multiplica quantidade e preço e adiciona a uma soma total
+//depois de percorrer tudo ele imprime o valor total da lista
 double ListaCompras::calculaTotal() const {
     if (listaVazia()) {
         cout << "Lista vazia, nao ha produtos para calcular." << endl;
